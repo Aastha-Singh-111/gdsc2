0d9e272895ba9d206bbfe42606286f9ac92bf255
Aastha-Singh-111 <98797127+Aastha-Singh-111@users.noreply.github.com>
2022-12-23 23:21:02 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,17 @@
+function navbarToggle() {
+    console.log("clicked");
+  
+    var element1 = document.getElementById("navBox");
+    element1.classList.toggle("navBox1");
+  
+    var element2 = document.getElementById("mid");
+    element2.classList.toggle("mid1");
+  }
+  
+  
+  var ele = document.getElementById('animation');
+  ele.style.position='absolute'
+  ele.style.top='10%'
+  ele.style.right='-3%'
+
+
